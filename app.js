ecdac77f5fda741afbc9ee1c5cafd7445e901404
mathewsandicliffe <mathew.james@sandicliffe.co.uk>
2020-11-06 00:57:12 +0000

    update to player variable

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const socket = require('./app/sockets/socket');
 const routes = require('./app/routes/routes')(app, express, __dirname);
 
 // Player component.
-const player = require('./app/game/player/player');
+const player = require('./app/game/entities/player/player');
 
 
 // Start the server and make it listen on selected port.

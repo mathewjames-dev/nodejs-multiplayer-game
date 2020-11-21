690ad2459841e95ae6159ccce45e7012acba86b1
mathewjames-dev <enquiries@mathewjames.dev>
2020-11-21 20:51:28 +0000

    Updated some global constants.

--- public/scripts/components/game.js
+++ public/scripts/components/game.js
@@ -3,15 +3,6 @@
  * Front-end Game Component
  *
  ***/
-
-// Setting up the variables.
-const mapCanvas = document.getElementById('mapContainer');
-const mapContext = mapCanvas.getContext("2d");
-const playerCanvas = document.getElementById('playerContainer');
-const playerContext = playerCanvas.getContext("2d");
-//const uiCanvas = document.getElementById('uiContainer');
-//const uiContext = uiCanvas.getContext("2d");
-
 // Setting up the components.
 const render = require('./map/render');
 const player = require('./player/player');
@@ -23,7 +14,8 @@ render.map.loadMap('map1');
 /*
  * Player event listeners for the main game.
  */
-// This is emitted from the server 60 times a second. This is so that we can update the players at 60fps.
+// This is emitted from the server 30 times a second. 
+// This is so that we can update the players at 30fps.
 socket.on('playersState', function (players) {
     player.updatePlayersState(playerContext, players);
 });
--- public/scripts/constants.js
+++ public/scripts/constants.js
@@ -7,6 +7,12 @@
 // Socket IO.
 global.socket = io();
 
+// Element Related Constants
+global.mapCanvas = document.getElementById('mapContainer');
+global.mapContext = mapCanvas.getContext("2d");
+global.playerCanvas = document.getElementById('playerContainer');
+global.playerContext = playerCanvas.getContext("2d");
+
 // Player Related Constants.
 global.playerIsAuthenticated = false;
 global.playerUsername = '';

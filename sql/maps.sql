aa8eca9a1b7516e9df2f84eca043030dd9557704
mathewjames-dev <enquiries@mathewjames.dev>
2020-11-22 20:27:35 +0000

    SQL Structure Dump

---
+++ sql/maps.sql
@@ -0,0 +1,33 @@
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : Local
+ Source Server Type    : MySQL
+ Source Server Version : 100406
+ Source Host           : localhost:3306
+ Source Schema         : mathewjamesdev_nodegame
+
+ Target Server Type    : MySQL
+ Target Server Version : 100406
+ File Encoding         : 65001
+
+ Date: 22/11/2020 20:25:51
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for maps
+-- ----------------------------
+DROP TABLE IF EXISTS `maps`;
+CREATE TABLE `maps`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp(0) NULL DEFAULT NULL,
+  `updated_at` timestamp(0) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(0),
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `location` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;

12f96868cd3d6920c05f735ab9dc0b4ec4f16c4e
mathewjames-dev <enquiries@mathewjames.dev>
2020-12-08 20:04:08 +0000

    Removed get player function so updated socket server accordingly.

--- app/socketServer.js
+++ app/socketServer.js
@@ -15,6 +15,7 @@ class SocketServer {
         this.io.sockets.on('connection', (socket) => {
             console.log('*** SERVER: NEW CONNECTION! ***');
 
+            this.sockets[socket.id] = socket;
             var socketId = socket.id;
 
             /*
@@ -25,30 +26,31 @@ class SocketServer {
                 console.log('*** SERVER: DISCONNECTED USER! ***');
 
                 // Start by removing the player from both the game and socket server.
-                let player = gameServer.game.getPlayer(socketId);
-
-                gameServer.game.removePlayer(socketId)
-                    .then(gameServer.socketServer.removeSocket(socketId))
-                    .then(player.updatePlayerState());
+                let player = gameServer.game.players[socketId];
+                if (!player) {
+                    gameServer.socketServer.removeSocket(socketId);
+                } else {
+                    gameServer.game.removePlayer(socketId)
+                        .then(gameServer.socketServer.removeSocket(socketId))
+                        .then(player.updatePlayerState());
+                }
             });
 
             /*
              * PLAYER EVENT LISTENERS.
              */
             socket.on('playerMovement', (data) => {
-                let player = gameServer.game.getPlayer(socketId);
-                if (player) {
-                    player.movePlayer(data)
-                }
+                let player = gameServer.game.players[socketId];
+                if (player) player.movePlayer(data)
             });
 
             socket.on('increaseHealth', (data) => {
-                let player = gameServer.game.getPlayer(data.id);
+                let player = gameServer.game.players[socketId];
                 if (player) player.health += data.health;
             });
 
             socket.on('removeItemFromInventory', (data) => {
-                let player = gameServer.game.getPlayer(data.id);
+                let player = gameServer.game.players[socketId];
                 player.removeItemFromInventory(data);
             })
 
@@ -63,19 +65,9 @@ class SocketServer {
         });
     }
 
-    // Add a socket to the list.
-    addSocket(id) {
-        this.sockets.push(id);
-    }
-
     // Remove a socket from the list.
     async removeSocket(id) {
         delete this.sockets[id];
-    }
-
-    // Function to get all sockets.
-    getSocketList() {
-        return this.sockets;
     }
 }
 

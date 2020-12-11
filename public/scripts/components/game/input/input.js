311dc8ae2cdf25e4be0ea3268f03080b23ad235f
mathewjames-dev <enquiries@mathewjames.dev>
2020-12-11 00:30:05 +0000

    Left, Up, Right and Down arrow also capable of moving player now.

--- public/scripts/components/game/input/input.js
+++ public/scripts/components/game/input/input.js
@@ -19,20 +19,33 @@ class Input {
     addKeyDownListener() {
         // Key down event listener.
         document.addEventListener('keydown', function (event) {
+            event.preventDefault();
             switch (event.keyCode) {
                 case 65: // A
+                    game.input.left = true;
+                    break;
+                case 37: // Left arrow.
                     game.input.left = true;
                     break;
 
                 case 87: // W
                     game.input.up = true;
                     break;
+                case 38: // Up arrow.
+                    game.input.up = true;
+                    break;
 
                 case 68: // D
                     game.input.right = true;
                     break;
+                case 39: // Right arrow.
+                    game.input.right = true;
+                    break;
 
                 case 83: //S
+                    game.input.down = true;
+                    break;
+                case 40: // Down arrow.
                     game.input.down = true;
                     break;
             }
@@ -43,20 +56,33 @@ class Input {
     addKeyUpListener() {
         // Key up event listener.
         document.addEventListener('keyup', function (event) {
+            event.preventDefault();
             switch (event.keyCode) {
                 case 65: // A
+                    game.input.left = false;
+                    break;
+                case 37: // Left arrow.
                     game.input.left = false;
                     break;
 
                 case 87: // W
                     game.input.up = false;
                     break;
+                case 38: // Up arrow.
+                    game.input.up = false;
+                    break;
 
                 case 68: // D
                     game.input.right = false;
                     break;
+                case 39: // Right arrow.
+                    game.input.right = false;
+                    break;
 
                 case 83: //S
+                    game.input.down = false;
+                    break;
+                case 40: // Down arrow.
                     game.input.down = false;
                     break;
             }

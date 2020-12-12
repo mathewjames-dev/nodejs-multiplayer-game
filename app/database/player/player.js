d3e98878992156cfa72973cfd271a72107292830
mathewjames-dev <enquiries@mathewjames.dev>
2020-12-12 18:54:22 +0000

    Remove unneeded function within the player database class.
    Implement a function to get players inventory items, and updated the get player inventory id with get player inventory (we can use it for multiple purposes this way)

--- app/database/player/player.js
+++ app/database/player/player.js
@@ -10,6 +10,7 @@ class PlayerDatabase extends Database {
         super();
     }
 
+    // Function to get the players user record by username.
     async getPlayer(username, callback) {
         let sql = "SELECT * from users WHERE username = ?";
         this.connection.query(sql, [username], function (err, result, fields) {
@@ -18,43 +19,46 @@ class PlayerDatabase extends Database {
         });
     }
 
-    async getPlayerInventoryId(username, callback) {
-        await this.getPlayer(username, (player) => {
-            let sql = "SELECT inventory.id FROM inventory " +
-                "WHERE user_id = ? ";
-            this.connection.query(sql, [player[0].id], function (err, result, fields) {
-                if (err) callback(false);
-                callback(result);
-            })
-        })
-    }
-
+    // Function to get the players inventory record.
     async getPlayerInventory(username, callback) {
-        let sql = "SELECT inv.max_size, item.* FROM users " +
+        let sql = "SELECT inv.* FROM users " +
             "INNER JOIN inventory inv ON users.id = inv.id " +
-            "LEFT JOIN inventory_items itemPivot on inv.id = itemPivot.inventory_id " +
-            "LEFT JOIN items item on itemPivot.item_id = item.id " +
-            "WHERE username = ?"
+            "WHERE username = ?";
         this.connection.query(sql, [username], function (err, result, fields) {
             if (err) callback(false);
             callback(result);
         });
     }
 
-    async updatePlayerState(player) {
-        let sql = "UPDATE users SET health = ?, x = ?, y = ? " +
-            "WHERE username = ?";
-        this.connection.query(sql, [player.health, player.x, player.y, player.username], function (err, result, fields) {
+    // Function to get the items within the players inventory.
+    async getPlayerInventoryItems(inventoryId, callback) {
+        let sql = "SELECT items.* FROM items " +
+            "INNER JOIN inventory_items invItems ON items.id = invItems.item_id " +
+            "WHERE invItems.inventory_id = ?";
+        this.connection.query(sql, [inventoryId], function (err, result, fields) {
+            if (err) callback(false);
+            callback(result);
         });
     }
+   // "LEFT JOIN inventory_items itemPivot on inv.id = itemPivot.inventory_id " +
+        //"LEFT JOIN items item on itemPivot.item_id = item.id " +
 
+    // Function to remove an item from a players inventory by item id (First instance)
     async removeItemFromInventory(player, itemId) {
         let $this = this;
-        this.getPlayerInventoryId(player.username, (inventory) => {
+        this.getPlayerInventory(player.username, (inventory) => {
             let sql = "DELETE from inventory_items WHERE item_id = ? AND inventory_id = ? LIMIT 1";
             $this.connection.query(sql, [itemId, inventory[0].id], function (err, result, fields) {
                 if (err) console.log(err);
             });
+        });
+    }
+
+    // Function to update the players state in the database.
+    async updatePlayerState(player) {
+        let sql = "UPDATE users SET health = ?, x = ?, y = ? " +
+            "WHERE username = ?";
+        this.connection.query(sql, [player.health, player.x, player.y, player.username], function (err, result, fields) {
         });
     }
 }

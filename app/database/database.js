d8b3fb5718d4676a5f3d915bfbf5ea5354b3bd61
mathewjames-dev <enquiries@mathewjames.dev>
2020-12-29 18:20:46 +0000

    Updated database file to utilize mongoose.

--- app/database/database.js
+++ app/database/database.js
@@ -3,22 +3,21 @@
  * Back-end Database Component File
  *
  ***/
-const mysql = require('mysql');
+const mongoose = require('mongoose');
 
 class Database {
     constructor() {
-        this.host = "localhost";
-        this.user = "root";
-        this.password = "";
+        this.uri = "mongodb://127.0.0.1:27017/" + process.env.DB_NAME;
 
         // Setup the connection to the database.
-        this.connection = mysql.createConnection({
-            host: this.host,
-            user: this.user,
-            password: this.password,
-            database: "mathewjamesdev_nodegame"
+        this.connection = mongoose.connect(this.uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
         });
-        this.connection.connect();
+            
+        this.db = mongoose.connection;
+        //Bind connection to error event (to get notification of connection errors)
+        this.db.on('error', console.error.bind(console, 'MongoDB connection error:'));
     }
 }
 
